10e042c7d1087c4f965a010fafd5861f4a3afb60
ammriteshh <amritesh6767@gmail.com>
2024-10-25 00:03:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Simple scroll animation when clicking on a section
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
